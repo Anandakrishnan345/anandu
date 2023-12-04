d79599114e13510ba59414e94f1764a711638ca5
Anandakrishnan345 <ckanandu@gmail.com>
2023-12-04 15:55:33 +0530

    array and loop

--- 1-12-23/script.js
+++ 1-12-23/script.js
@@ -189,3 +189,196 @@ let locarr=[];
  console.log("uppercase",upcarr)
  console.log("lowercase",locarr)
 }
+{
+    let strr=''
+    let strr2='' //print in viewport
+    let i;
+    let j;
+    for (i=1;i<=10;i++){
+        for(j=1;j<=i;j++){
+            strr=strr+'*';
+            strr2=strr2+'*';
+        }
+        strr=strr+'\n';
+        strr2=strr2+'<br>';
+        
+    }
+    
+    console.log(strr)
+    document.write(strr2,)
+}
+
+{
+    //4-12-23
+    //functions
+
+    //function definition
+    function printhello(){
+        console.log("hello world")
+        console.log('hai')
+    }
+    printhello();//functon call
+
+    //named function
+    function getAge(){
+        return 12;
+
+    }
+    let age =getAge();
+    console.log("getAge",age);
+
+    //Anonymous function
+    let getName=function(){
+        console.log("get name function called");
+        return "john";
+    }
+let name=getName();
+console.log("Name :",name);
+
+
+//Arrow function 
+let getMarks=()=>{
+    console.log("Arrow function called");
+    return 70;
+
+}
+let mark=getMarks();
+console.log("maks :",mark);
+}
+
+{
+  //redo all the above programs using function 
+
+  // sum of array using function
+  function sum(){
+    let arr =[50,30,10,40,20];
+     let sum=0;
+     for(let i=0;i<arr.length;i++)
+     {
+        sum=sum+arr[i];
+     }   
+     console.log("arraysum: ",sum);
+
+   
+     
+  }
+  sum()
+
+//find small element in array using function
+  function min(){
+    let arr =[50,30,10,40,20];
+    let min = arr[0];
+    for(i=0;i<arr.length;i++){
+       if(arr[i]<min){
+           min=arr[i];
+       }
+    }
+
+    console.log("min: ",min)
+  }
+ min()
+
+
+//find large element in array using function
+
+let arr =[50,30,10,40,20];
+ function max(arr){
+    
+    let max = arr[0];
+    for(i=0;i<arr.length;i++){
+       if(arr[i]>max){
+           max=arr[i];
+       }
+    }
+
+    console.log("max: ",max)
+ }
+ max(arr)
+
+//find fibonocci series using function
+
+ function fibonocci(){
+    console.log("fibonocci series");
+    let sum1=0;
+    let limit=10;
+    let a =0;
+    let b=1;
+    let l=0;
+    while(l<=limit){
+        sum1=sum1+a;
+        console.log(sum1);
+        a=b;
+        b=sum1;
+        l++;
+    }
+ }
+ fibonocci()
+
+ //find factorial using function with parameter 
+
+ function factorial(num){
+    let fact=1;
+    
+    for( let i=num;i>0;i--){fact=fact*i}
+    console.log("factorial is:",fact)
+ }
+ factorial(5)
+
+ //string reverse using function with parameter 
+
+ function message(str){
+    
+let reverse = '';
+
+for (let i=str.length-1;i>=0;i--){
+    reverse=reverse+str[i];
+
+}
+    console.log("reverse",reverse);
+ }
+
+message('hello')
+
+
+
+function star(){
+    let todo='using function';
+    document.write(todo)
+    document.write("<br>")
+    
+    let strr=''
+    let strr2='' //print in viewport
+    let i;
+    let j;
+    for (i=1;i<=10;i++){
+        for(j=1;j<=i;j++){
+            strr=strr+'*';
+            strr2=strr2+'*';
+        }
+        strr=strr+'\n';
+        strr2=strr2+'<br>';
+        
+    }
+    
+    console.log(strr)
+    document.write(strr2,)
+}
+star()
+
+}
+{
+     
+    let text='aea'
+    let count = 0;  
+    for(let i =0;i<text.length;i++){
+        if(text[i]=='a'){
+            count=1;
+            console.log(count)
+            
+
+        }
+        // rree=[count]
+        // console.log(rree)
+    }
+    // console.log("sum",sum)
+}
