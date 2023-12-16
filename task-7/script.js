99ade02f1ddf729e3bf78ebc31950744f7518ae0
Anandakrishnan345 <ckanandu@gmail.com>
2023-12-16 15:55:50 +0530

    updated

--- task-7/script.js
+++ task-7/script.js
@@ -11,7 +11,8 @@
     xhr.onreadystatechange = function (){
         console.log("ready sate :",xhr.readyState);
         console.log("statusCode :",xhr.status);
-        let content = document.getElementById('content')
+        let content = document.getElementById('content');
+       
         
         
         if(xhr.readyState==4){
@@ -25,7 +26,7 @@
             let parsed_response = JSON.parse(response);
             console.log("parsed_response:",parsed_response);
             console.log("typeof parsed_response",typeof(parsed_response));
-
+            
             datas = '';
             
             for(let i = 0;i<parsed_response.length;i++){
@@ -35,7 +36,7 @@
                 <td>${parsed_response[i].title}</td>
                 <td>${parsed_response[i].price}</td>
                 <td>${parsed_response[i].description}</td>
-                <td><img src="${parsed_response[i].image}" alt=""> </td>
+                <td><img src="${parsed_response[i].image}" alt="" width="100px" height="50px"> </td>
                 
                 </tr>
                 `
