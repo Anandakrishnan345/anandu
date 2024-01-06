30e6dc151ddcd63a225a6537a6598118cec52a14
Anandakrishnan345 <ckanandu@gmail.com>
2024-01-06 16:09:32 +0530

    node

--- NODE/client/script.js
+++ NODE/client/script.js
@@ -1,6 +1,9 @@
 
 
 async function getuserData() {
+   
+
+
     let userData = await fetch('http://localhost:3000/getData');
     console.log("userData :",userData);
     let parsedUserData = await userData.json();
@@ -10,12 +13,26 @@ async function getuserData() {
      for(let i=0;i<parsedUserData.length;i++){
         content=content +`
             <tr>
-            <td>${parsedUserData[i].name}</td>
-            <td>${parsedUserData[i].email}</td>
-            <td>${parsedUserData[i].password}</td>
+            <td>${parsedUserData[i]._id}</td>
+            <td><input type="name" name="name" id="name-${parsedUserData[i]._id}" value=${parsedUserData[i].name} disabled="true"></td> 
+            <td><input type="email" name="email" id="email-${parsedUserData[i]._id}" value=${parsedUserData[i].email} disabled="true"></td>
+            <td><input type="password" name="password" id="name-${parsedUserData[i]._id}" value=${parsedUserData[i].password} disabled="true"></td>
+            <td><button onclick="handleEdit('${parsedUserData[i]._id}')">Edit</button></td>
             </tr>
         `
      }
      tbody.innerHTML=content;
 }
 getuserData();
+function handleEdit(id){
+    let _id = id;
+    console.log("id :",_id)
+
+    let name = document.getElementById(`name-${_id}`);
+    console.log("name :",name);
+    name.disabled=false;
+
+    let email = document.getElementById(`email-${_id}`);
+    console.log("email :",email);
+    email.disabled=false;
+}
